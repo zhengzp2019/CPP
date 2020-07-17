d74700a41119e0f8c0e99a71770ffae21a9b3f36
Dapeng DP <44330233@qq.com>
2020-07-17 13:11:26 +0800

    update

--- 6_18.cpp
+++ 6_18.cpp
@@ -8,11 +8,12 @@ private:
     int x, y;
 
 public:
-    Point();//默认构造函数
-    Point(int xx, int yy);//自定义构造函数
+    Point();                       //默认构造函数
+    Point(int xx, int yy);         //自定义构造函数
+    void move(int newX, int newY); //将点坐标输入point对象
     Point(Point &p);
     ~Point();
-    void move(int newX, int newY)//将点坐标输入point对象
+    void move(int newX, int newY)
     {
         x = newX;
         y = newY;
@@ -47,10 +48,10 @@ private:
     Point *points;
 
 public:
-    ArrayOfPoint(int size);//生成一个长度为size的point数组
-    ArrayOfPoint(ArrayOfPoint &ps);//arrayofpoint的构造函数
+    ArrayOfPoint(int size);         //生成一个长度为size的point数组
+    ArrayOfPoint(ArrayOfPoint &ps); //arrayofpoint的构造函数
     ~ArrayOfPoint();
-    Point &element(int index)//输出下标为index的对象
+    Point &element(int index) //输出下标为index的对象
     {
         assert(index >= 0 && index < size); //如果数组越界则停止
         return points[index];
@@ -62,12 +63,12 @@ ArrayOfPoint::ArrayOfPoint(int size) : size(size)
     points = new Point[size];
 }
 
-ArrayOfPoint::ArrayOfPoint(ArrayOfPoint &ps):size(ps.size)
+ArrayOfPoint::ArrayOfPoint(ArrayOfPoint &ps) : size(ps.size)
 {
     points = new Point[size];
-    for (int i = 0; i < size;i++)
+    for (int i = 0; i < size; i++)
     {
-        points[i] = ps.points[i];//注意：point类的复制构造函数存在才行
+        points[i] = ps.points[i]; //注意：point类的复制构造函数存在才行
     }
 }
 
@@ -79,11 +80,11 @@ ArrayOfPoint::~ArrayOfPoint()
 
 int main()
 {
-    int count;//数组长度
+    int count; //数组长度
     cout << "please enter the counter of point:";
     cin >> count;
     ArrayOfPoint points(count);
-    points.element(0).move(5,0);
+    points.element(0).move(5, 0);
     points.element(1).move(15, 20);
     return 0;
 }
